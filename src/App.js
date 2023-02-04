ef86108abd47e52f80a7159664da08e3be7fae9b
Gowtham Rajesh <gowtham@konnectify.co>
2023-02-04 11:38:59 +0530

    login-successfully-v2

--- src/App.js
+++ src/App.js
@@ -3,33 +3,166 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="logo">
-        <img src="./images/ZEE5_logo.png" className="img-logo" alt="" />
-      </div>
-      <div className="starhub-logo">
-        <img src="./images/starhub-logo.png" className="str-logo" alt="" />
-      </div>
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-        <div className="subscribe-msg">
-          <p>You have logged in successfully</p>
+    <div>
+    <div className="overlay"></div>
+    <div className="subscribe">
+      <div className="subscribe-container">
+        <div className="subscribe--main">
+          <div className="subscribe-container-item"></div>
+          <div className="subscribe-container-item">
+            <label htmlFor="" className="subscribe-label">
+              Mobile
+            </label>
+          </div>
+          <div className="subscribe-container-item">
+            <label htmlFor="" className="subscribe-label">
+              Premium HD
+            </label>
+          </div>
+        </div>
+        <hr />
+
+        <div className="subscribe--main">
+          <div className="subscribe-container-item">
+            <label htmlFor="" className="subscribe-label">
+              Premium content
+            </label>
+          </div>
+          <div className="subscribe-container-item">
+            <label htmlFor="" className="subscribe-label">
+              {" "}
+              ✓
+            </label>
+          </div>
+          <div className="subscribe-container-item">
+            <label htmlFor="" className="subscribe-label">
+              ✓
+            </label>
+          </div>
+        </div>
+        <hr />
+
+        <div className="subscribe--main">
+          <div className="subscribe-container-item">
+            <label htmlFor="" className="subscribe-label">
+              Ad-free
+            </label>
+          </div>
+          <div className="subscribe-container-item">
+            <label htmlFor="" className="subscribe-label">
+              ✓
+            </label>
+          </div>
+          <div className="subscribe-container-item">
+            <label htmlFor="" className="subscribe-label">
+              ✓
+            </label>
+          </div>
+        </div>
+        <hr />
+
+        <div className="subscribe--main">
+          <div className="subscribe-container-item">
+            <label htmlFor="" className="subscribe-label">
+              Device
+            </label>
+          </div>
+          <div className="subscribe-container-item">
+            <label htmlFor="" className="subscribe-label">
+              ✓
+            </label>
+          </div>
+          <div className="subscribe-container-item">
+            <label htmlFor="" className="subscribe-label">
+              ✓
+            </label>
+          </div>
+        </div>
+        <hr />
+
+        <div className="subscribe--main">
+          <div className="subscribe-container-item">
+            <label htmlFor="" className="subscribe-label">
+              No. of screens
+            </label>
+          </div>
+          <div className="subscribe-container-item">
+            <label htmlFor="" className="subscribe-label">
+              1
+            </label>
+          </div>
+          <div className="subscribe-container-item">
+            <label htmlFor="">2</label>
+          </div>
         </div>
 
-      </header>
+        <div className="subscribe-msg">
+          <p>You have subscribed Zee5. Please enjoy the subscription.</p>
+        </div>
+      </div>
     </div>
+
+<div className="main">
+<div className="header">
+  <div className="images">
+    <div className="image">
+      <img src="images/Valimai.jpg" className="img" alt="" />
+    </div>
+    <div className="image">
+      <img src="images/nerkonda.jpg" className="img" alt="" />
+    </div>
+    <div className="image">
+      <img src="images/lona.jpg" className="img" alt="" />
+    </div>
+    <div className="image">
+      <img src="images/mersal1.jpg" className="img" alt="" />
+    </div>
+    <div className="image">
+      <img src="images/kanaa.jpg" className="img" alt="" />
+    </div>
+    <div className="image">
+      <img src="images/igloo.jpg" className="img" alt="" />
+    </div>
+    <div className="image">
+      <img src="images/Sardar.jpg" className="img" alt="" />
+    </div>
+    <div className="image">
+      <img src="images/thunivu.jpg" className="img" alt="" />
+    </div>
+  </div>
+</div>
+<div className="rest"></div>
+</div>
+</div>
   );
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <div className="logo">
+  //       <img src="./images/ZEE5_logo.png" className="img-logo" alt="" />
+  //     </div>
+  //     <div className="starhub-logo">
+  //       <img src="./images/starhub-logo.png" className="str-logo" alt="" />
+  //     </div>
+  //       {/* <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a> */}
+  //       <div className="subscribe-msg">
+  //         <p>You have logged in successfully</p>
+  //       </div>
+
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
